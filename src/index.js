8eb276b8667d14a31124b8f4d6c411a4ee01099f
Ryan Bowman <[email protected]>
2024-08-23 10:21:17 -0400

    Created initial layout of project with a title, headline underneath and a button for the user to click to start inputting items onto their todo list.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import "./style.css";
-alert("hello");
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    renderForm();
+    })
+    
+    const mainContent = document.querySelector('.content');
+    const pageTitle = document.createElement('h1');
+    pageTitle.innerHTML = "Todo List";
+    mainContent.appendChild(pageTitle);
+    
+    const pageHeadline = document.createElement('h4');
+    pageHeadline.classList.add('page-headline')
+    pageHeadline.innerHTML = "Conquer today!";
+    mainContent.appendChild(pageHeadline);
+
+    const todoBtn = document.createElement('button')
+    todoBtn.classList.add('todo-btn');
+    todoBtn.innerHTML = "Add item to list"
+    mainContent.appendChild(todoBtn);
+    
+    function renderForm(){
+        todoBtn.addEventListener('click', ()=>{
+            //creates form
+            const todoForm = document.createElement('form');
+            
+            //creates input items
+            const inputName = document.createElement('input');
+            inputName.type = 'text';
+            inputName.name = 'name';
+            inputName.placeholder = 'name'
+    
+            mainContent.appendChild(todoForm);
+            todoForm.appendChild(inputName)
+        })
+    }
 
