eb14bc1382d902dd145b54c69317eea93f35041c
Ryan Bowman <[email protected]>
2024-08-27 00:27:38 -0400

    Styled app to make more visually appealing

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const headerContent = document.querySelector('.header-content');
     const pageTitle = document.createElement('h1');
     pageTitle.setAttribute('id', 'page-title');
-    pageTitle.innerHTML = "To-do List";
+    pageTitle.innerHTML = "GoalGetter";
     headerContent.appendChild(pageTitle);
     
     const pageHeadline = document.createElement('h4');
     pageHeadline.setAttribute('id', 'page-headline');
-    pageHeadline.innerHTML = "Conquer Today!";
+    pageHeadline.innerHTML = "Achieve More with Every Checkmark";
     headerContent.appendChild(pageHeadline);
 
     const todoBtn = document.createElement('button')
@@ -44,48 +44,53 @@ document.addEventListener('DOMContentLoaded', ()=>{
             todoForm.classList.add('todo-form');
             //creates input items
             const inputTitle = document.createElement('input');
-            inputTitle.placeholder = 'title'
+            inputTitle.placeholder = 'Title'
             inputTitle.setAttribute('type', 'text');
             inputTitle.setAttribute('name', 'title');
             inputTitle.setAttribute('id', 'title');
+            inputTitle.classList.add('form-inputs');
 
             const inputDescription = document.createElement('input');
-            inputDescription.placeholder = 'description of task';
+            inputDescription.placeholder = 'Description';
             inputDescription.setAttribute('type', 'text');
             inputDescription.setAttribute('name', 'description');
             inputDescription.setAttribute('id', 'description');
-
+            inputDescription.classList.add('form-inputs');
+            
             const inputDueDate = document.createElement('input');
-            inputDueDate.placeholder = 'Due by:';
             inputDueDate.setAttribute('type', 'date');
             inputDueDate.setAttribute('name', 'due-date');
             inputDueDate.setAttribute('id', 'due-date');
+            inputDueDate.classList.add('form-inputs');
 
             const inputCategory = document.createElement('input');
             inputCategory.maxLength = 40;
-            inputCategory.placeholder = 'category';
+            inputCategory.placeholder = 'Category';
             inputCategory.setAttribute('type', 'text');
             inputCategory.setAttribute('name', 'category');
             inputCategory.setAttribute('id', 'category');
+            inputCategory.classList.add('form-inputs');
 
             const inputPriority = document.createElement('input');
-            inputPriority.placeholder = '1(lowest) - 5(highest)'
+            inputPriority.placeholder = '1(low) - 5(high)'
             inputPriority.setAttribute('type', 'number');
             inputPriority.setAttribute('name', 'priority');
             inputPriority.setAttribute('id', 'priority');
+            inputPriority.classList.add('form-inputs');
 
             const inputNotes = document.createElement('input');
             inputNotes.max = 100;
-            inputNotes.placeholder = 'Notes about task(optional)';
+            inputNotes.placeholder = 'Notes';
             inputNotes.setAttribute('type', 'text');
             inputNotes.setAttribute('name', 'notes');
             inputNotes.setAttribute('id', 'notes');
+            inputNotes.classList.add('form-inputs');
 
             const formSubmitBtn = document.createElement('input');
             formSubmitBtn.placeholder = 'Submit';
             formSubmitBtn.setAttribute('type', 'submit');
             formSubmitBtn.setAttribute('id', 'form-submit-btn');
-
+            
             //appends all input items into the todo form
             todoForm.appendChild(inputTitle);
             todoForm.appendChild(inputDescription);
