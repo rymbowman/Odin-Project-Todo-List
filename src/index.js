da596ef0976750a96d62b1ec0e9c676abe579cd4
Ryan Bowman <[email protected]>
2024-08-31 13:57:00 -0400

    created a helper function to make the displayForm() easier to read

--- src/index.js
+++ src/index.js
@@ -1,16 +1,115 @@
 import "./style.css";
-import { renderHeader, renderBody } from "./render-document";
 
 document.addEventListener("DOMContentLoaded", () => {
   renderHeader();
   renderBody();
 });
 
-function CreateTask(title, description, dueDate, category, priority, notes) {
+function renderHeader() {
+  const headerContent = document.querySelector(".header-content");
+  const pageTitle = document.createElement("h1");
+  const svg =
+    '<svg width="110px" height="110px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.2086 9.70558C18.5983 9.31421 18.5969 8.68105 18.2056 8.29137C17.8142 7.90169 17.1811 7.90305 16.7914 8.29442L11.0215 14.0892L7.70214 10.816C7.30888 10.4283 6.67573 10.4327 6.28796 10.8259C5.90018 11.2192 5.90461 11.8524 6.29787 12.2401L10.3258 16.212C10.7177 16.5985 11.3483 16.5956 11.7366 16.2056L18.2086 9.70558Z" fill="#50C878"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M4.5 2C3.11929 2 2 3.11929 2 4.5V19.5C2 20.8807 3.11929 22 4.5 22H19.5C20.8807 22 22 20.8807 22 19.5V4.5C22 3.11929 20.8807 2 19.5 2H4.5ZM4 4.5C4 4.22386 4.22386 4 4.5 4H19.5C19.7761 4 20 4.22386 20 4.5V19.5C20 19.7761 19.7761 20 19.5 20H4.5C4.22386 20 4 19.7761 4 19.5V4.5Z" fill="#50C878"></path> </g></svg>';
+  pageTitle.setAttribute("id", "page-title");
+  pageTitle.innerHTML = `${svg}CheckBox`;
+  headerContent.appendChild(pageTitle);
+
+  const pageHeadline = document.createElement("h4");
+  pageHeadline.setAttribute("id", "page-headline");
+  pageHeadline.innerHTML = "Achieve More with Every Checkmark";
+  headerContent.appendChild(pageHeadline);
+
+  const addTaskBtn = document.createElement("button");
+  addTaskBtn.classList.add("todo-btn");
+  addTaskBtn.innerHTML = "Add Task";
+  headerContent.appendChild(addTaskBtn);
+  addTaskBtn.addEventListener("click", displayForm);
+}
+
+function renderBody() {
+  const newTaskContent = document.querySelector(".tasks-content");
+
+  const incompletedTasks = document.createElement("div");
+  incompletedTasks.classList.add("incompleted-tasks-container");
+  const incompletedTasksTitle = document.createElement("h2");
+  incompletedTasksTitle.innerHTML = "Incomplete";
+  newTaskContent.appendChild(incompletedTasks);
+  incompletedTasks.appendChild(incompletedTasksTitle);
+
+  const completedTasks = document.createElement("div");
+  completedTasks.classList.add("completed-tasks-container");
+  const completedTasksTitle = document.createElement("h2");
+  completedTasksTitle.innerHTML = "Complete";
+  newTaskContent.appendChild(completedTasks);
+  completedTasks.appendChild(completedTasksTitle);
+}
+
+function createInput({ type, name, id, placeholder = "", maxLength }) {
+  const input = document.createElement("input");
+  input.setAttribute("type", type);
+  input.setAttribute("name", name);
+  input.setAttribute("id", id);
+  if (placeholder) input.placeholder = placeholder;
+  if (maxLength) input.maxLength = maxLength;
+  input.classList.add("form-inputs");
+  return input;
+}
+
+function displayForm() {
+  const headerContent = document.querySelector(".header-content");
+  //creates form
+  const todoForm = document.createElement("form");
+  todoForm.classList.add("todo-form");
+
+  const inputs = [
+    {
+      type: "text",
+      name: "title",
+      id: "title-input",
+      placeholder: "Title",
+      maxLength: 50,
+    },
+    { type: "date", name: "due-date", id: "date-input" },
+    {
+      type: "number",
+      name: "priority",
+      id: "priority-input",
+      placeholder: "1(low) - 5(high)",
+      maxLength: 1,
+    },
+    {
+      type: "text",
+      name: "notes",
+      id: "notes-input",
+      placeholder: "Notes",
+      maxLength: 250,
+    },
+  ];
+
+  inputs.forEach((inputItem) => {
+    const inputElement = createInput(inputItem);
+    todoForm.appendChild(inputElement);
+  });
+
+  const formSubmitBtn = createInput({
+    type: "submit",
+    name: "form-submit",
+    id: "form-submit-btn",
+    placeholder: "Submit",
+  });
+  todoForm.appendChild(formSubmitBtn);
+
+  todoForm.addEventListener("submit", (event) => {
+    console.log("form was submitted");
+    formSubmit(event, todoForm);
+  });
+
+  headerContent.appendChild(todoForm);
+}
+
+function CreateTask(title, dueDate, priority, notes) {
   this.title = title;
-  this.description = description;
   this.dueDate = dueDate;
-  this.category = category;
   this.priority = priority;
   this.notes = notes;
 }
@@ -20,19 +119,15 @@ function formSubmit(event, form) {
   event.preventDefault();
 
   //creates variables for each value from the form item
-  let titleValue = document.getElementById("title").value;
-  let descriptionValue = document.getElementById("description").value;
-  let dueDateValue = document.getElementById("due-date").value;
-  let categoryValue = document.getElementById("category").value;
-  let priorityValue = document.getElementById("priority").value;
-  let notesValue = document.getElementById("notes").value;
+  let titleValue = document.getElementById("title-input").value;
+  let dueDateValue = document.getElementById("date-input").value;
+  let priorityValue = document.getElementById("priority-input").value;
+  let notesValue = document.getElementById("notes-input").value;
 
   //sets variable 'newTask' equal to the object constructor for creating tasks
   let newTask = new CreateTask(
     titleValue,
-    descriptionValue,
     dueDateValue,
-    categoryValue,
     priorityValue,
     notesValue
   );
@@ -49,9 +144,10 @@ function formSubmit(event, form) {
                 <input type="checkbox" class="completion-check">
                 <label>Complete</label>
             </div>`;
-
+  const incompletedTasks = document.querySelector(
+    ".incompleted-tasks-container"
+  );
   incompletedTasks.appendChild(newPost);
-
   form.reset();
   form.remove();
   /*
