7760ae443b7a6950fbbcb59cca1ad889a0b5a46b
Ryan Bowman <[email protected]>
2024-08-31 15:09:14 -0400

    updated the styling of the info button on each new task

--- src/form-handler.js
+++ src/form-handler.js
@@ -82,7 +82,7 @@ export function formSubmit(event, form) {
   );
 
   const infoIcon =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: scaleX(-1);msFilter:progid:DXImageTransform.Microsoft.BasicImage(rotation=0, mirror=1);"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M11 11h2v6h-2zm0-4h2v2h-2z"></path></svg>';
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);filter: brightness(150%);transition: filter 0.3s;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M11 11h2v6h-2zm0-4h2v2h-2z"></path></svg>';
   let newPost = document.createElement("div");
   newPost.classList.add("new-post");
   newPost.innerHTML = `<div>${newTask.title}</div>
